7d1fbaac7025fdbadbcc5581b43e75f54ee37381
Kaori <xiaxulizhaiteng@gmail.com>
2018-12-22 17:12:50 +0900

    modelを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'blogs#index'
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
